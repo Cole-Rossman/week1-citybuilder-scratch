11f7f697132bbf1bd560b3258f1fe1e47b9ca3d2
Cole Rossman <colerossman99@gmail.com>
2022-01-27 15:52:56 -0800

    makestatsstring function and test created

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,11 @@
-import './utils.js';
+import { makeStatsString } from '../utils.js';
+
+const test = QUnit.test;
+
+test('should return a string describing the current state', (expect) => {
+    const expected = `You have clicked on the character selector 3 times, the environment selector 8 times, and the opponent selector 12 times. Your champion fought hard:`;
+
+    const actual = makeStatsString(3, 8, 12);
+
+    expect.equal(actual, expected);
+});
