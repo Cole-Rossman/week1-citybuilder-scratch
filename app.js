1b054832eb3a8ab68f06f6fc4eee67f3014037c2
Cole Rossman <colerossman99@gmail.com>
2022-01-27 16:50:58 -0800

    Imported functions and dom elements grabbed. Let state also complete.

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 // import functions and grab DOM elements
+import { makeStatsString } from './utils.js';
+
+const characterSelect = document.getElementById('character-select');
+const environmentSelect = document.getElementById('environment-select');
+const opponentSelect = document.getElementById('opponent-select');
+
+const characterImageEl = document.getElementById('character-image');
+const environmentImageEl = document.getElementById('environment-image');
+const opponentImageEl = document.getElementById('opponent-image');
+const captionImageEl = document.getElementById('caption');
+
+const captionList = document.getElementById('caption-list');
+const captionInput = document.getElementById('caption-input');
+const captionButton = document.getElementById('caption-button');
 
 // let state
+let characterCount = 0;
+
+let environmentCount = 0;
+
+let opponentCount = 0;
+
+let captions = [];
 
 // set event listeners 
+characterSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+
+    characterCount++;
+
+    characterImageEl.style.backgroundImage = `url("./assets/character-${value}.png)`;
+
+    displayStats();
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,9 @@
-import { makeStatsString } from '../utils.js';
+import { makeStatsString } from './utils.js';
 
 const test = QUnit.test;
 
 test('should return a string describing the current state', (expect) => {
-    const expected = `You have clicked on the character selector 3 times, the environment selector 8 times, and the opponent selector 12 times. Your champion fought hard:`;
+    const expected = `You have clicked the character selector 3 times, the environment selector 8 times, and the opponent selector 12 times. Your champion fought hard:`;
 
     const actual = makeStatsString(3, 8, 12);
 
